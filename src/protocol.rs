fa5616075fa1d36c43c32dd70333e6915b83b539
Elichai Turkel <elichai.turkel@gmail.com>
2022-02-01 21:18:56 +0200

    Update tests

--- src/protocol.rs
+++ src/protocol.rs
@@ -394,11 +394,8 @@ fn edwards_from_bytes(bytes: &[u8]) -> Option<EdwardsPoint> {
 #[cfg(test)]
 pub(crate) mod tests {
     use crate::protocol::{
-        aggregate_partial_signatures, aggregate_public_keys, generate_partial_nonces_internal,
-        partial_sign,
+        generate_partial_nonces_internal, AggPublicKeyAndMusigCoeff, KeyPair, Signature,
     };
-    use crate::protocol::{ExpandedKeyPair, Signature};
-    use curve25519_dalek::edwards::EdwardsPoint;
     use curve25519_dalek::scalar::Scalar;
     use ed25519_dalek::Verifier;
     use hex::decode;
@@ -414,13 +411,9 @@ pub(crate) mod tests {
         Xoshiro256PlusPlus::seed_from_u64(seed)
     }
 
-    pub fn verify_dalek(pk: &EdwardsPoint, sig: &Signature, msg: &[u8]) -> bool {
-        let mut sig_bytes = [0u8; 64];
-        sig_bytes[..32].copy_from_slice(sig.R.compress().as_bytes());
-        sig_bytes[32..].copy_from_slice(sig.s.as_bytes());
-
-        let dalek_pub = ed25519_dalek::PublicKey::from_bytes(pk.compress().as_bytes()).unwrap();
-        let dalek_sig = ed25519_dalek::Signature::from_bytes(&sig_bytes).unwrap();
+    pub fn verify_dalek(pk: [u8; 32], sig: [u8; 64], msg: &[u8]) -> bool {
+        let dalek_pub = ed25519_dalek::PublicKey::from_bytes(&pk).unwrap();
+        let dalek_sig = ed25519_dalek::Signature::from_bytes(&sig).unwrap();
 
         dalek_pub.verify(msg, &dalek_sig).is_ok()
     }
@@ -432,16 +425,12 @@ pub(crate) mod tests {
         let mut privkey = [0u8; 32];
         for _ in 0..4096 {
             rng.fill_bytes(&mut privkey);
-            let zengo_keypair = ExpandedKeyPair::create_from_private_key(privkey);
+            let zengo_keypair = KeyPair::create_from_private_key(privkey);
             let dalek_secret = ed25519_dalek::SecretKey::from_bytes(&privkey)
                 .expect("Can only fail if bytes.len()<32");
             let dalek_pub = ed25519_dalek::PublicKey::from(&dalek_secret);
 
-            let zengo_compressed_pub = zengo_keypair.public_key.compress();
-            let zengo_pub_serialized = zengo_compressed_pub.as_bytes();
-            let dalek_pub_serialized = dalek_pub.to_bytes();
-
-            assert_eq!(zengo_pub_serialized, &dalek_pub_serialized);
+            assert_eq!(zengo_keypair.pubkey(), dalek_pub.to_bytes());
         }
     }
 
@@ -454,11 +443,10 @@ pub(crate) mod tests {
             "c7d17a93f129527bf7ca413f34a0f23c8462a9c3a3edd4f04550a43cdd60b27a";
         let expected_pubkey: [u8; 32] = decode(expected_pubkey_hex).unwrap().try_into().unwrap();
 
-        let keypair = ExpandedKeyPair::create_from_private_key(priv_dec);
-        let pubkey = keypair.public_key;
+        let keypair = KeyPair::create_from_private_key(priv_dec);
         assert_eq!(
-            pubkey.compress().as_bytes(),
-            &expected_pubkey,
+            keypair.pubkey(),
+            expected_pubkey,
             "Public keys do not match!"
         );
     }
@@ -466,7 +454,7 @@ pub(crate) mod tests {
     #[test]
     fn test_multiparty_signing_for_two_parties() {
         let mut rng = deterministic_fast_rand("test_multiparty_signing_for_two_parties", None);
-        for _i in 0..50 {
+        for _ in 0..50 {
             test_multiparty_signing_for_two_parties_internal(&mut rng);
         }
     }
@@ -475,8 +463,8 @@ pub(crate) mod tests {
         let message: [u8; 12] = [79, 77, 69, 82, 60, 61, 100, 156, 109, 125, 3, 19];
 
         // generate signing keys and partial nonces
-        let party0_key = ExpandedKeyPair::create();
-        let party1_key = ExpandedKeyPair::create();
+        let party0_key = KeyPair::create_from_private_key(rng.gen());
+        let party1_key = KeyPair::create_from_private_key(rng.gen());
 
         let (p0_private_nonces, p0_public_nonces) =
             generate_partial_nonces_internal(&party0_key, Option::Some(&message), rng);
@@ -484,60 +472,63 @@ pub(crate) mod tests {
             generate_partial_nonces_internal(&party1_key, Option::Some(&message), rng);
 
         // compute aggregated public key:
-        let party0_key_agg =
-            match aggregate_public_keys(&party0_key.public_key, &party1_key.public_key) {
-                Some(pub_key_agg) => pub_key_agg,
-                None => panic!("Both public keys are the same"),
-            };
-        let party1_key_agg =
-            match aggregate_public_keys(&party1_key.public_key, &party0_key.public_key) {
-                Some(pub_key_agg) => pub_key_agg,
-                None => panic!("Both public keys are the same"),
-            };
+        let party0_key_agg = AggPublicKeyAndMusigCoeff::aggregate_public_keys(
+            party0_key.pubkey(),
+            party1_key.pubkey(),
+        )
+        .unwrap();
+        let party1_key_agg = AggPublicKeyAndMusigCoeff::aggregate_public_keys(
+            party1_key.pubkey(),
+            party0_key.pubkey(),
+        )
+        .unwrap();
         assert_eq!(party0_key_agg.agg_public_key, party1_key_agg.agg_public_key);
         assert!(
-            (party0_key_agg.musig_coefficient == Scalar::one()
-                || party1_key_agg.musig_coefficient == Scalar::one())
-                && (party0_key_agg.musig_coefficient != Scalar::one()
-                    || party1_key_agg.musig_coefficient != Scalar::one())
+            party0_key_agg.musig_coefficient == Scalar::one()
+                || party1_key_agg.musig_coefficient == Scalar::one()
+        );
+        assert!(
+            party0_key_agg.musig_coefficient != Scalar::one()
+                || party1_key_agg.musig_coefficient != Scalar::one()
         );
         // Compute partial signatures
-        let s0 = partial_sign(
-            &p1_public_nonces,
-            &p0_public_nonces,
-            &p0_private_nonces,
+        let (s0, aggregated_nonce0) = party0_key.partial_sign(
+            p0_private_nonces,
+            [p1_public_nonces.clone(), p0_public_nonces.clone()],
             &party0_key_agg,
-            &party0_key,
             &message,
         );
-        let s1 = partial_sign(
-            &p0_public_nonces,
-            &p1_public_nonces,
-            &p1_private_nonces,
+        let (s1, aggregated_nonce1) = party1_key.partial_sign(
+            p1_private_nonces,
+            [p0_public_nonces, p1_public_nonces],
             &party1_key_agg,
-            &party1_key,
             &message,
         );
+        assert_eq!(aggregated_nonce0, aggregated_nonce1);
+        assert_eq!(aggregated_nonce0.serialize(), aggregated_nonce1.serialize());
 
-        let signature0 = aggregate_partial_signatures(&s0, &s1.my_partial_s);
-        let signature1 = aggregate_partial_signatures(&s1, &s0.my_partial_s);
-        assert!(s0.R == s1.R, "Different partial nonce aggregation!");
-        assert!(signature0.s == signature1.s);
+        let signature0 =
+            Signature::aggregate_partial_signatures(aggregated_nonce0, [s0.clone(), s1.clone()]);
+        let signature1 =
+            Signature::aggregate_partial_signatures(aggregated_nonce1, [s1.clone(), s0.clone()]);
+        assert_eq!(signature0, signature1);
+        assert_eq!(signature0.serialize(), signature1.serialize());
         // debugging asserts
-        assert!(
-            s0.my_partial_s + s1.my_partial_s == signature0.s,
-            "signature aggregation failed!"
-        );
+        assert_eq!(s0.0 + s1.0, signature0.s, "signature aggregation failed!");
         // verify:
         assert!(
             signature0
-                .verify(&message, &party0_key_agg.agg_public_key)
+                .verify(&message, party0_key_agg.aggregated_pubkey())
                 .is_ok(),
             "Verification failed!"
         );
         // Verify result against dalek
         assert!(
-            verify_dalek(&party0_key_agg.agg_public_key, &signature0, &message),
+            verify_dalek(
+                party0_key_agg.aggregated_pubkey(),
+                signature0.serialize(),
+                &message
+            ),
             "Dalek signature verification failed!"
         );
     }
